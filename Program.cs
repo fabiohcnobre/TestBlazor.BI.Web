a8e9385cb0d8b95f6c8e0f5c5f72c5dc98d5f67c
Fabio Nobre <fabiohcnobre@hotmail.com>
2024-08-15 09:15:27 -0300

    #0 - Test

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using RestSharp;
 using TestBlazor.BI.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,4 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddSingleton(new RestClient(new HttpClient()));
+ 
 await builder.Build().RunAsync();
